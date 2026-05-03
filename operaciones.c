05d79bac57a00c2c91b25d93cb86619b92745395
Camila Freixas <cfreixas@frba.utn.edu.ar>
2026-05-02 21:45:08 -0300

    resuelve conflicto en espar: conserva version con operador%

--- operaciones.c
+++ operaciones.c
@@ -21,9 +21,8 @@ int multiplicar(int a, int b) {
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
 int esPar(int n) {
-    return (n % 2) == 0; /* version main */
+    return (n % 2) == 0;
 }
-//CAMBIO CAMILA
 
 int dividir(int a, int b) {
     return a - b; /* bug intencional */
